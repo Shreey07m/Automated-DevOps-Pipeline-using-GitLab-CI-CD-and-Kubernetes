7894eaa8b052c632ba3c6bc82a501d5d38d5a9e8
Shreeya Maliye <1329@arthfin.com>
2026-06-29 20:02:36 +0530

    add project on gitlab

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps Project Running 🚀"
+
+@app.route("/health")
+def health():
+    return jsonify(status="UP")
+
+@app.route("/users")
+def users():
+    return jsonify(users=["Shreeya", "DevOps", "Cloud"])
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
